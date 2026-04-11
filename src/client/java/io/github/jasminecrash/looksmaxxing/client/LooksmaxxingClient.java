d76831c6f7302e42c1520a0b778c3b54634e8693
JasmineCrash <jazzminecrash@gmail.com>
2026-04-11 13:02:24 -0700

    initial commit

---
+++ src/client/java/io/github/jasminecrash/looksmaxxing/client/LooksmaxxingClient.java
@@ -0,0 +1,10 @@
+package io.github.jasminecrash.looksmaxxing.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LooksmaxxingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
